8fff39f38066e3612d812560e3482454fd2e3065
Omm Sahoo <osahoo9178@gmail.com>
2025-09-29 16:43:17 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/app/student/notes/page.tsx
+++ src/app/student/notes/page.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { notes } from '@/lib/db';
-import { getSession } from '@/lib/auth';
+import { protectPage } from '@/lib/auth';
 import type { Student } from '@/lib/types';
 import { File } from 'lucide-react';
 
@@ -14,7 +14,7 @@ const iconMap = {
 }
 
 export default async function StudentNotesPage() {
-  const { user } = await getSession();
+  const user = await protectPage('student');
   const student = user as Student;
 
   const filteredNotes = notes.filter(note => note.class === student.course && note.section === student.section);
